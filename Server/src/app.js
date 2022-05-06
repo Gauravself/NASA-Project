8eb9a3bdd6850667eb6d3daf506c11dfe735299a
Gaurav Rastogi <rastogi.g1998@gmail.com>
2022-05-06 08:48:17 +0530

    Adding routes using MVC model in express

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const planetRouter = require('./routes/planets/planets.router');
 
 app.use(express.json());
+app.use(planetRouter);
 
 module.exports = app;
