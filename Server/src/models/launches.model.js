ff91becebbd7aff53eea210a9213a12afade5c6e
Gaurav Rastogi <rastogi.g1998@gmail.com>
2022-05-13 10:57:04 +0530

    Improvements

--- Server/src/models/launches.model.js
+++ Server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
   mission: "Kepler Exploration x",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
-  destination: "Kepler-442 b",
+  target: "Kepler-442 b",
   customer: ["Gaurav", "ISRO"],
   upcoming: true,
   success: true,
--- Server/src/routes/launches/launches.controller.js
+++ Server/src/routes/launches/launches.controller.js
@@ -10,12 +10,11 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
 
-  if(!launch.date || !launch.mission || !launch.rocket || !launch.destination){
+  if(!launch.launchDate || !launch.mission || !launch.rocket || !launch.target){
       return res.status(400).json({
           error:'Bad Request, Missing Data or Invalid Data'
       })
   }
-
   launch.launchDate = new Date(launch.launchDate);
   addNewLaunches(launch);
   return res.status(201).json(launch);
